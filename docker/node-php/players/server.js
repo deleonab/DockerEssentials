bc6830db984c71ad3a51db49f0f5d8c7a192fb93
Dele Onabowu <delesodade@hotmail.com>
2022-08-18 01:03:53 +0100

    updated code for container running php

--- docker/node-php/players/server.js
+++ docker/node-php/players/server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 // Set host to default node host to accept all connections
 const HOST = "0.0.0.0";
-const PORT = "80";
+const PORT = 80;
 
 // we shall now create an endpoint
 
 app.get('/', (req,res)=>{
 
-res.json({});
-    players: ['Victor','Dele','Oruenene','Mia','Bella','Abi']
+res.send({
+
+    players:['Victor','Dele','Oruenene','Mia','Bella','Abi']
+});
+   
 
 });
 
